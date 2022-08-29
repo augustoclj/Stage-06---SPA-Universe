e8618ee9291a2ea472892f9288af2f86ec8d9db1
Augusto Julietti <augusto.julietti@gmail.com>
2022-08-29 19:10:06 -0300

    Add files via upload

---
+++ js/router.js
@@ -0,0 +1,35 @@
+export class Router{
+  routes = {}
+
+  add(routeName, page){
+    this.routes[routeName] = page
+  }
+
+  route(event){
+    event = event || window.event
+    event.preventDefault()
+
+    window.history.pushState({},"", event.target.href)
+
+    this.handle()
+  }
+
+  handle(){
+    const { pathname } = window.location
+    const route = this.routes[pathname] || this.routes[404]
+    fetch(route)
+    .then(data => data.text())
+    .then(html => {
+
+      if(document.querySelector("nav a.active") !== null){
+        document.querySelector("nav a.active").classList.remove("active")
+        document.getElementById(pathname).classList.add("active")
+      }else{
+        document.getElementById(pathname).classList.add("active")
+      }
+
+      document.querySelector('#app').innerHTML = html
+
+    })
+  }
+}
